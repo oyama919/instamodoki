25ad19c50bf3781657d55a9ef5160b7c43d1a847
oyama919 <oyama919@gmail.com>
2016-11-06 20:49:04 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  root 'blogs#index'
+end
