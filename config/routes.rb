bd2fe0c62c677ab7a7a032f64418877060a4fbf6
oyama919 <oyama919@gmail.com>
2016-11-06 21:38:18 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  # devise_for :users, controllers: {
-  #   omniauth_callbacks: "users/omniauth_callbacks"
-  # }
+  devise_for :users
 
-  devise_for :users, controllers: {
-  omniauth_callbacks: "users/omniauth_callbacks"
-}
+  # devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  #   delete '/users/sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+  #
+  # devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  # devise_scope :users do
+  #   delete '/users/sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
+
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy]
 
   root 'blogs#index'
