e8e5e38e1c7d764858d346a83b160f8b69a0cbe3
oyama919 <oyama919@gmail.com>
2016-11-19 17:26:16 +0900

    syusei

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -37,7 +37,7 @@ class PicturesController < ApplicationController
 
 private
   def pictures_params
-    params.require(:picture).permit(:title,:image)
+    params.require(:picture).permit(:title,:image,:content)
   end
   def set_picture
     @picture = Picture.find(params[:id])
