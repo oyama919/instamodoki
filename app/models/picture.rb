25437010ee425ca7cf53d2e77bab889529e6861c
oyama919 <oyama919@gmail.com>
2016-11-20 18:31:11 +0900

    blogDELETE

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ActiveRecord::Base
+    validates :title, presence: true
+    validates :image, presence: true
     belongs_to :user
     mount_uploader :image, ToukouimageUploader #deviseの設定配下に追記
 end
