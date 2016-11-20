018b6d9e569bfaee54a9eec2037bdb9b2761e77e
oyama919 <oyama919@gmail.com>
2016-11-20 18:46:05 +0900

    dekitakana

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20161114112335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
-    t.string   "name"
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
     t.string   "image"
